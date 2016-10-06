7415be2c61f287e27f1e9c3821300ca559a6ad9a
Ron Hashimoto <mail@h2so5.net>
2016-10-07 01:04:47 +0900

    x

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export CC=gcc-5
   export CXX=g++-5
   export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
+  Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
   sleep 3
 
   export ELECTRON_VERSION=`jq .devDependencies.electron package.json -r`
