404a6a9dd0dcb8788c5aaef8561a1a319549c96b
Ron Hashimoto <mail@h2so5.net>
2016-10-06 14:58:57 +0900

    x

--- filter_thread.cpp
+++ filter_thread.cpp
@@ -81,16 +81,13 @@ FilterThread::Private::Private(const std::shared_ptr<Context> &ctx) : ctx(ctx) {
             break;
           if (ctx.maxSeq < ctx.store->maxSeq()) {
             uint32_t seq = ++ctx.maxSeq;
-            lock.unlock();
             const std::shared_ptr<Packet> &pkt = ctx.store->get(seq);
-            lock.lock();
 
+            /*
             v8::Handle<v8::Value> args[1] = {
                 v8pp::class_<Packet>::reference_external(isolate, pkt.get())};
             v8::Local<v8::Value> result =
                 func->Call(isolate->GetCurrentContext()->Global(), 1, args);
-
-            v8pp::class_<Packet>::unreference_external(isolate, pkt.get());
             if (result.IsEmpty()) {
               if (ctx.errorCb) {
                 Nan::Utf8String str(try_catch.Exception());
@@ -102,6 +99,7 @@ FilterThread::Private::Private(const std::shared_ptr<Context> &ctx) : ctx(ctx) {
               if (ctx.updateCb)
                 ctx.updateCb(pkt->seq());
             }
+            */
           }
         }
       }
