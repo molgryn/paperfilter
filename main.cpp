2381cb07d7bcaa69a66f930c036e6b9c9b3871ab
Ron Hashimoto <mail@h2so5.net>
2016-10-15 14:27:06 +0900

    na

--- main.cpp
+++ main.cpp
@@ -5,27 +5,10 @@
 #include "session_packet_wrapper.hpp"
 #include "session_wrapper.hpp"
 #include <nan.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <sys/un.h>
-#include <unistd.h>
-#include <execinfo.h>
 
 using namespace v8;
 
 void Init(v8::Local<v8::Object> exports) {
-  signal(SIGSEGV, [](int sig) {
-    void *array[256];
-    size_t size = backtrace(array, 256);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(1);
-});
-std::set_terminate([]() {
-    void *array[256];
-    size_t size = backtrace(array, 256);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(1);
-});
   SessionPacketWrapper::Init(exports);
   SessionLayerWrapper::Init(exports);
   SessionItemWrapper::Init(exports);
